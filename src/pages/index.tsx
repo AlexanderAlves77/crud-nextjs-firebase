245f9168f1b245fcbc416b7ad228242b2d813c4d
Alexander Alves <vendas77.rj@gmail.com>
2022-09-28 17:27:28 -0300

    Criando o componente Tabela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,32 @@
 import Layout from "../components/Layout"
+import Tabela from "../components/Tabela"
+import Cliente from "../core/Cliente"
 
 export default function Home() {
+  const clientes = [
+    new Cliente('Sara', 8, '1'),
+    new Cliente('Kelvin', 21, '2'),
+    new Cliente('Joaquim', 3, '3'),
+    new Cliente('Patrick', 19, '4'),
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(`Selecionado...${cliente.nome}`)
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluir...${cliente.nome}`)
+  }
+
   return (
     <div className={`flex h-screen justify-center items-center bg-gradient-to-r
       from-blue-600 to-purple-500`}>
-        
+
      <Layout titulo="Cadastro Simples">
-       <span>Conteúdo</span>
+       <Tabela clientes={clientes} 
+        clienteSelecionado={clienteSelecionado}
+        clienteExcluido={clienteExcluido}
+      />
      </Layout>
     </div>
   )
