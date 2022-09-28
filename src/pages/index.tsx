5272c440e06f9214ac50ec42ab5b516985370a67
Alexander Alves <vendas77.rj@gmail.com>
2022-09-28 16:37:48 -0300

    Criando os componentes Layout e Titulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
-
+import Layout from "../components/Layout"
 
 export default function Home() {
   return (
-    <div className={``}>
-     
+    <div className={`flex h-screen justify-center items-center bg-gradient-to-r
+      from-blue-600 to-purple-500`}>
+        
+     <Layout titulo="Cadastro Simples">
+       <span>Conteúdo</span>
+     </Layout>
     </div>
   )
 }
+
